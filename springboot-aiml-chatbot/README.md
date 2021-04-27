3799c92c5ccb5d28bd90874deccdd133c9052564
srikanth-josyula <josyulasrikanth94@gmail.com>
2021-04-27 18:52:51 +0530

    springboot-aiml

---
+++ springboot-aiml-chatbot/README.md
@@ -0,0 +1,2 @@
+# springboot-aiml-chatbot
+Java Based AIML(Artificial Intelligence Marker Language) Sample code with REST controller
---
+++ springboot-aiml-chatbot/src/main/java/com/sample/dao/UserDaoService.java
@@ -0,0 +1,40 @@
+package com.sample.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sample.model.User;
+
+@Component
+public class UserDaoService {
+
+	static List<User> users = new ArrayList<>();
+
+	static int usersCount = 3;
+
+	static {
+		users.add(new User(1, "admin"));
+		users.add(new User(2, "user"));
+		users.add(new User(3, "kanth"));
+	}
+
+	public User findOne(int id) {
+		for (User user : users) {
+			if (user.getUserid() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public User findbyName(String name) {
+		for (User user : users) {
+			if (user.getUsername().equalsIgnoreCase(name)) {
+				return user;
+			}
+		}
+		return null;
+	}
+}
