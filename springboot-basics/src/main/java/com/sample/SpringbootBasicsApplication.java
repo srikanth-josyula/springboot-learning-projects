9f6aa45302890d59072c43a542609eb040bf264f
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-07-01 14:31:34 -0300

    Update SpringbootBasicsApplication.java

--- springboot-basics/src/main/java/com/sample/SpringbootBasicsApplication.java
+++ springboot-basics/src/main/java/com/sample/SpringbootBasicsApplication.java
@@ -11,16 +11,12 @@ public class SpringbootBasicsApplication implements CommandLineRunner {
 	@Value("${app.message}")
 	private String message;
 
-	@Value("${name}")
-	private String name;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBasicsApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello, " + name + "!");
 		System.out.println(message);
 	}
 }
